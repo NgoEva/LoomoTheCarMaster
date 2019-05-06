384b97a0efb26fea7b01cbdca1bb218164c6e80a
Eva Ngo <evango@kth.se>
2019-05-06 21:45:37 +0200

    little app using speech, recognition, base and vls sdk

---
+++ src/main/java/com/segway/loomo/Spot.java
@@ -0,0 +1,27 @@
+package com.segway.loomo;
+
+public class Spot {
+    private float x_coordinate;
+    private float y_coordinate;
+
+    public Spot(float x_coordinate, float y_coordinate) {
+        this.x_coordinate = x_coordinate;
+        this.y_coordinate = y_coordinate;
+    }
+
+    public float getX_coordinate() {
+        return x_coordinate;
+    }
+
+    public void setX_coordinate(float x_coordinate) {
+        this.x_coordinate = x_coordinate;
+    }
+
+    public float getY_coordinate() {
+        return y_coordinate;
+    }
+
+    public void setY_coordinate(float y_coordinate) {
+        this.y_coordinate = y_coordinate;
+    }
+}
