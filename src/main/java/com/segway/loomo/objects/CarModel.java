d1a494d8076bdf1b26cc296f1a5930ee624873bd
NinaScherer <33720930+NinaScherer@users.noreply.github.com>
2019-05-10 15:11:28 +0200

    Klasse CarModel befuellt

--- src/main/java/com/segway/loomo/objects/CarModel.java
+++ src/main/java/com/segway/loomo/objects/CarModel.java
@@ -1,4 +1,28 @@
 package com.segway.loomo.objects;
 
 public class CarModel extends AppObject {
+    private String name;
+    private int category_id;
+
+    public CarModel (String name, int category_id) {
+
+        this.name = name;
+        this.category_id = category_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(int category_id) {
+        this.category_id = category_id;
+    }
 }
