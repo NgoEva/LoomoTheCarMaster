39afeaa3335261cfe0228651754cc47a07b1003b
thomaslehenberger <33665678+thomaslehenberger@users.noreply.github.com>
2019-05-13 22:11:14 +0200

    Car AppObject

--- src/main/java/com/segway/loomo/objects/Category.java
+++ src/main/java/com/segway/loomo/objects/Category.java
@@ -1,4 +1,19 @@
 package com.segway.loomo.objects;
 
 public class Category extends AppObject {
+
+    private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
